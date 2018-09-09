6c9660f0c8b3f4e71d509d00efcff1b10e3aabd1
Emir Kotan <emirkotan@gmail.com>
2018-09-09 22:16:47 +0100

    draft mock/stream.js

---
+++ test/mock/stream.js
@@ -0,0 +1,44 @@
+const Func = require("utils/func.js");
+const Stream = require("utils/stream.js");
+
+function makeReq(method, path) {
+	const req = Stream.emptyReadable();
+	req.method = method;
+	req.url = path;
+	return req;
+}
+
+exports.createGET = (path) =>
+	makeReq("GET", path);
+
+function makeResInbound(code, body) {
+	const res = Stream.readableWith(body);
+	res.statusCode = code;
+	res.headers = {};
+	return res;
+}
+
+exports.respondBody = (body) =>
+	makeResInbound(200, body);
+
+exports.respondCode = (code) =>
+	makeResInbound(code, "");
+
+exports.respondWith = makeResInbound;
+
+function makeResOutbound(done) {
+	const res = Stream.writableBuffered(done);
+	res.writeHead = function (code, headers) {
+		res.statusCode = code;
+		res.headers = headers;
+	};
+	return res;
+}
+
+exports.responseBuffered = makeResOutbound;
+exports.responseEnd = function (done) {
+	const res = makeResOutbound(() => done(res));
+	return res;
+};
+exports.responseDumped = () =>
+	makeResOutbound(Func.empty);

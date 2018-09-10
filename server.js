27a68e12415bdd784f013aa550d18de39dc4bfb2
Emir Kotan <emirkotan@gmail.com>
2018-09-10 21:17:00 +0100

    server.js only passes routes to handle()

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const http = require("http");
 const config = require("./config.json");
 const handle = require("./src/handle.js");
 
+const routes = config.target.routes;
 const port = Number(process.argv[2]) || config.port;
 
 const print = () =>
 	console.log("Listening port", port);
 
-http.createServer(handle(config)).listen(port, print);
+http.createServer(handle(routes)).listen(port, print);

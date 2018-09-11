274113936b258ef9f4859d91b1ec2000021b907a
Emir Kotan <emirkotan@gmail.com>
2018-09-11 22:01:30 +0100

    Func.side(Cache.trySet)

--- src/use-cache.js
+++ src/use-cache.js
@@ -3,11 +3,6 @@ const Cache = require("./cache.js");
 const Fw = require("./forward.js");
 const race = require("./race.js");
 
-const tryToCache = (req) => function (res) {
-	Cache.trySet(req, res);
-	return res;
-};
-
 const tryToRace = (req, cache) => (err) =>
 	err.statusCode
 		? race(req, cache)
@@ -17,6 +12,6 @@ module.exports = function (req, res) {
 	const cache = Cache.toResponse(req).catch(Func.empty);
 	return Fw.requestOk(req)
 		.catch(tryToRace(req, cache))
-		.then(tryToCache(req))
+		.then(Func.side(Cache.trySet(req)))
 		.then(Fw.response(res));
 };

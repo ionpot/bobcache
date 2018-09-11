604a3b3aa87b1d82e9b8d6e53e5211bdf8cdb0dc
Emir Kotan <emirkotan@gmail.com>
2018-09-11 22:01:37 +0100

    forward.js

---
+++ src/forward.js
@@ -0,0 +1,29 @@
+const cond = require("utils/cond.js");
+const Func = require("utils/func.js");
+const Client = require("./client.js");
+
+const is2XX = (res) =>
+	Math.floor(res.statusCode / 100) === 2;
+
+const ifOk = (yes, no) =>
+	cond(is2XX, yes, no);
+
+const checkIfOk =
+	ifOk(Func.id, (res) => Promise.reject(res));
+
+exports.request = (req) =>
+	Client.forward(req);
+
+exports.requestOk = (req) =>
+	Client.get(req).then(checkIfOk);
+
+exports.requestUntilOk = (req) =>
+	new Promise(function self(done, fail) {
+		const retry = () => self(done, fail);
+		Client.get(req)
+			.then(ifOk(done, retry))
+			.catch(fail);
+	});
+
+exports.response = (dest) => (res) =>
+	res.pipe(dest.writeHead(res.statusCode, res.headers));

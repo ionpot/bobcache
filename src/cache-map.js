3f0ba64e581e0bedcb9caf83f1f49f46b6c6dda6
Emir Kotan <emirkotan@gmail.com>
2018-09-12 22:43:06 +0100

    cache-map.js

---
+++ src/cache-map.js
@@ -0,0 +1,11 @@
+const P = require("utils/promise.js");
+
+const map = new Map();
+const fetch = (key) => map.get(key);
+const has = (key) => map.has(key);
+const store = (key, val) => void map.set(key, val);
+
+exports.fetch = (key) =>
+	P.cond(has)(key).then(fetch);
+
+exports.store = store;

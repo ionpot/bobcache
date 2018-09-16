cae341b9a981fb7940e6100be7b9eeb3cfcb8c31
Emir Kotan <emirkotan@gmail.com>
2018-09-16 02:13:52 +0100

    remove no-cache.js

--- src/handle.js
+++ src/handle.js
@@ -1,23 +1,22 @@
+const cond = require("utils/cond.js");
 const RegEx = require("utils/regexp.js");
 const Str = require("utils/str.js");
 const Fw = require("./forward.js");
 const useCache = require("./use-cache.js");
-const noCache = require("./no-cache.js");
 
 const matches = (routes, url) =>
 	routes
 		.map(str => RegEx.makeFull(str))
 		.some(RegEx.test(Str.noTrailingSlashes(url)));
 
-const canCache = (routes, req) =>
+const canCache = (routes) => (req) =>
 	req.method === "GET"
 	&& matches(routes, req.url);
 
-const handlerOf = (routes, req) =>
-	canCache(routes, req)
-		? useCache
-		: noCache;
+const request = (routes) =>
+	cond(canCache(routes))(useCache, Fw.request);
 
 module.exports = (routes) => (req, res) =>
-	handlerOf(routes, req)(req, res)
+	request(routes)(req)
+		.then(Fw.response(res))
 		.catch(Fw.error(res));
--- src/no-cache.js
+++
@@ -1,4 +0,0 @@
-const Fw = require("./forward.js");
-
-module.exports = (req, res) =>
-	Fw.request(req).then(Fw.response(res));
